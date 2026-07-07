468f74ea9a8eb77cac295a3439a25c52061627ec
stevenroyere-sys <stevenroyere@hotmail.fr>
2026-07-08 01:23:47 +0200

    Update config.py

--- config.py
+++ config.py
@@ -28,3 +28,68 @@ PRODUCTS = [
     },
     {
         "key": "portasplit_castorama",
+        "product": "Portasplit",
+        "site": "Castorama",
+        "url": "https://www.castorama.fr/climatiseur-portasplit-midea-reversible-3500w/8431312260509_CAFR.prd",
+    },
+    {
+        "key": "portasplit_leroymerlin",
+        "product": "Portasplit",
+        "site": "Leroy Merlin",
+        "url": "https://www.leroymerlin.fr/produits/climatiseur-split-mobile-reversible-portasplit-midea-par-optimea-93857579.html",
+    },
+    {
+        "key": "portasplit_joybuy",
+        "product": "Portasplit",
+        "site": "Joybuy",
+        "url": "https://www.joybuy.fr/dp/midea-portasplit-climatiseur-réversible-froidchaud-35kw/100003175005531",
+    },
+    {
+        "key": "portasplit_rakuten",
+        "product": "Portasplit",
+        "site": "Rakuten",
+        "url": "https://fr.shopping.rakuten.com/offer/buy/13466164647/clim-reversible-optimea-mmcs-12hrn8-qrd0.html",
+    },
+    {
+        "key": "portasplit_fnac",
+        "product": "Portasplit",
+        "site": "Fnac",
+        "url": "https://www.fnac.com/MIDEA-Climatiseur-Split-Mobile-Reversible-Froid-Chaud-3500W-12000BTU-WiFi-deshumidificateur-ventilateur-jusqu-a-40m2-kit-fenetre-inclus/a21457105/w-4",
+    },
+    {
+        "key": "portasplit_auchan",
+        "product": "Portasplit",
+        "site": "Auchan",
+        "url": "https://www.auchan.fr/optimea-clim-reversible-midea-climatiseur-split-mobile/pr-10b80596-1b4d-4ebb-aeba-ad08f0943013",
+    },
+]
+
+OUT_OF_STOCK_KEYWORDS = [
+    "rupture de stock",
+    "indisponible",
+    "actuellement indisponible",
+    "non disponible",
+    "épuisé",
+    "epuise",
+    "hors stock",
+    "en réapprovisionnement",
+    "produit épuisé",
+    "temporairement indisponible",
+    "sold out",
+    "out of stock",
+    "unavailable",
+    "notify me when available",
+    "prévenez-moi",
+    "être alerté",
+    "me prévenir",
+]
+
+IN_STOCK_KEYWORDS = [
+    "ajouter au panier",
+    "acheter maintenant",
+    "en stock",
+    "add to cart",
+    "buy now",
+    "livraison offerte",
+    "disponible en ligne",
+]
